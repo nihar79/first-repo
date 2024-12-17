0575d920778716c77fe24f60a41e71a297e7e12b
nihar79 <niharikatiwari151719@gmail.com>
2024-12-17 12:19:58 +0530

    notes added

---
+++ hello.py
@@ -0,0 +1,13 @@
+print("Hello World!")
+name = 'Niharika'
+#id will print the memory address
+print(id(name))
+a=['hello',1,6.8,'Python']
+print(a)
+a.append([8,9,10])
+print(a)
+a.reverse()
+print("A is now reversed a= ",a)
+print(a.pop(0))
+a.insert(0,[8,9,10])
+print(a)
