8abd4b352ff813c81ea7e2c510657509fed7a3ae
nihar79 <niharikatiwari151719@gmail.com>
2024-12-17 12:58:46 +0530

    added more files

---
+++ dictionary.py
@@ -0,0 +1,14 @@
+dict={id:101,"name":"python","loc":"bang"}
+print(type(dict))
+
+print(dict.keys())
+
+#multiline json format in dictionaries
+b1= {
+    101:"Java",
+    102:"Python",
+    103: {
+        "Python":"Pandas",
+        "Java":"Spring"
+    }
+}
---
+++ tuple.py
@@ -0,0 +1,4 @@
+tup=(8,9,10,'jk')
+print(tup)
+print(tup.count(8))
+print(tup.index('jk'))
